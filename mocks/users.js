ac2d6e06f6a839a1b3e6e7e57324b6649905fd56
Dinca <adrianflorin.dinca@finastra.com>
2019-12-17 15:29:50 +0200

    chore: Add different image sources

--- mocks/users.js
+++ mocks/users.js
@@ -16,27 +16,27 @@ export const usersData = {
     photos: [
       {
         src:
-          'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          'https://images.pexels.com/photos/3359742/pexels-photo-3359742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         alt: 'gallery photo',
       },
       {
         src:
-          'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          'https://images.pexels.com/photos/2004930/pexels-photo-2004930.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         alt: 'gallery photo',
       },
       {
         src:
-          'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          'https://images.pexels.com/photos/3350141/pexels-photo-3350141.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         alt: 'gallery photo',
       },
       {
         src:
-          'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          'https://images.pexels.com/photos/3336152/pexels-photo-3336152.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         alt: 'gallery photo',
       },
       {
         src:
-          'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          'https://images.pexels.com/photos/3210189/pexels-photo-3210189.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         alt: 'gallery photo',
       },
     ],
