5893f5763fc9c905219365755c9211dcaec34405
Adrian <adriandinca2008@yahoo.com>
2020-01-05 21:15:10 +0200

    feat: Add mocks for posts

---
+++ mocks/posts.js
@@ -0,0 +1,118 @@
+export const postsData = {
+  rd32Gf2L: {
+    author: {
+      id: 'e2H4aD3j1',
+    },
+    date: new Date('December 17, 2019 10:24:17'),
+    photos: [
+      {
+        src:
+          'https://images.pexels.com/photos/3359742/pexels-photo-3359742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'gallery photo',
+      },
+    ],
+    comments: [
+      {
+        id: '2aH1dFG3',
+        user: {
+          id: '',
+          name: '',
+          photo: {
+            src: '',
+            alt: '',
+          },
+        },
+        comment: 'Awesome photo',
+      },
+    ],
+    likes: 32,
+  },
+  K34jdF9r: {
+    author: {
+      id: 'e2H4aD3j1',
+    },
+    date: new Date('December 20, 2019 10:24:17'),
+    photos: [
+      {
+        src:
+          'https://images.pexels.com/photos/2004930/pexels-photo-2004930.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'gallery photo',
+      },
+      {
+        src:
+          'https://images.pexels.com/photos/3350141/pexels-photo-3350141.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'gallery photo',
+      },
+    ],
+    comments: [
+      {
+        id: 'W4f3aFS',
+        user: {
+          id: '',
+          name: '',
+          photo: {
+            src: '',
+            alt: '',
+          },
+        },
+        comment: 'Cool pic',
+      },
+      {
+        id: 'iJy5rhTss',
+        user: {
+          id: '',
+          name: '',
+          photo: {
+            src: '',
+            alt: '',
+          },
+        },
+        comment: 'Amazing',
+      },
+    ],
+    likes: 56,
+  },
+  a4Te0ik34: {
+    author: {
+      id: 'e2H4aD3j1',
+    },
+    date: new Date('December 24, 2019 10:24:17'),
+    photos: [
+      {
+        src:
+          'https://images.pexels.com/photos/3336152/pexels-photo-3336152.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'gallery photo',
+      },
+    ],
+    comments: [
+      {
+        id: '1ktRhM',
+        user: {
+          id: '',
+          name: '',
+          photo: {
+            src: '',
+            alt: '',
+          },
+        },
+        comment: 'Awesome',
+      },
+    ],
+    likes: 48,
+  },
+  Bn23fG345: {
+    author: {
+      id: 'e2H4aD3j1',
+    },
+    date: new Date('December 31, 2019 10:24:17'),
+    photos: [
+      {
+        src:
+          'https://images.pexels.com/photos/3210189/pexels-photo-3210189.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'gallery photo',
+      },
+    ],
+    comments: [],
+    likes: 62,
+  },
+};
