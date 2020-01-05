0be18d2474f5208fcfe4bc28a2970d7352060e0e
Adrian <adriandinca2008@yahoo.com>
2020-01-06 00:30:24 +0200

    feat: Add chat view

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { getChatData } from '../../services/chat';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
-const Chat = () => (
-  <>
-    <p>Hello Chat</p>
-  </>
-);
+const Chat = ({ messages, avatars }) => {
+  return (
+    <>
+      {messages.map(message => (
+        <Card key={message.date}>
+          <CardContent>
+            <Typography gutterBottom>{message.text}</Typography>
+          </CardContent>
+        </Card>
+      ))}
+      <p></p>
+    </>
+  );
+};
+
+Chat.getInitialProps = async context => {
+  const { id } = context.query;
+
+  const { messages, avatars } = await getChatData(id);
+
+  return { messages, avatars };
+};
 
 export default Chat;
