e890856c2ea4b307b9c2c046c37463febf34503b
Adrian <adriandinca2008@yahoo.com>
2020-01-06 00:29:43 +0200

    feat: Add chat mocks

---
+++ mocks/chats.js
@@ -0,0 +1,41 @@
+export const chatsData = {
+  erijtogfr: {
+    messages: [
+      {
+        senderId: 'e2H4aD3j1',
+        text: 'Lorem ipsum',
+        date: new Date('December 24, 2019 10:25:17'),
+      },
+      {
+        senderId: 'H46tFd5',
+        text: 'Dolor sit',
+        date: new Date('December 24, 2019 10:25:26'),
+      },
+      {
+        senderId: 'e2H4aD3j1',
+        text: 'Amet consectetur adipisicing elit',
+        date: new Date('December 24, 2019 10:25:39'),
+      },
+      {
+        senderId: 'H46tFd5',
+        text: 'Debitis, voluptatum?',
+        date: new Date('December 24, 2019 10:25:56'),
+      },
+      {
+        senderId: 'e2H4aD3j1',
+        text: 'Possimus, dolor unde!',
+        date: new Date('December 24, 2019 10:26:21'),
+      },
+      {
+        senderId: 'e2H4aD3j1',
+        text: 'Ab quos ut in facere nemo',
+        date: new Date('December 24, 2019 10:26:42'),
+      },
+      {
+        senderId: 'H46tFd5',
+        text: 'Sunt officia porro iste nihil',
+        date: new Date('December 24, 2019 10:27:02'),
+      },
+    ],
+  },
+};
