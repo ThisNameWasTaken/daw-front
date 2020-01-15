7294acc9759b737759529c8693efda9787f65093
Dinca <adrianflorin.dinca@finastra.com>
2020-01-15 14:37:33 +0200

    refactor: Split posts between users

--- mocks/users.js
+++ mocks/users.js
@@ -14,7 +14,7 @@ export const usersData = {
     username: 'John Doe',
     description:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A blanditiis voluptates ratione dolorum perspiciatis eum tenetur.',
-    posts: ['rd32Gf2L', 'K34jdF9r', 'a4Te0ik34', 'Bn23fG345'],
+    posts: ['K34jdF9r', 'a4Te0ik34'],
     chats: ['erijtogfr'],
   },
   H46tFd5: {
@@ -28,11 +28,11 @@ export const usersData = {
         'https://images.pexels.com/photos/3354641/pexels-photo-3354641.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       alt: 'cover photo',
     },
-    name: 'John Doe',
-    username: 'John Doe',
+    name: 'Jane Doe',
+    username: 'Jane Doe',
     description:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A blanditiis voluptates ratione dolorum perspiciatis eum tenetur.',
-    posts: ['rd32Gf2L', 'K34jdF9r', 'a4Te0ik34', 'Bn23fG345'],
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque et quidem sunt commodi enim quibusdam atque asperiores repellat maxime? Sit quibusdam a quasi ullam error explicabo et, dolores minus facilis.',
+    posts: ['rd32Gf2L', 'Bn23fG345'],
     chats: ['erijtogfr'],
   },
 };
